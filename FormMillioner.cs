3a2f5a48f7b67772ca862550c35581ae66519283
Емельдяев Антон <aemelia2004@gmail.com>
2025-03-05 10:46:00 +0500

    rg

--- FormMillioner.cs
+++ FormMillioner.cs
@@ -334,7 +334,7 @@ namespace ElementsDock
         private void buttonHall_Click(object sender, EventArgs e)
         {
             verBut2 = 1;
-            buttonHall.Enabled = false;//ей
+            buttonHall.Enabled = false;//ей7
             buttonFifty.Enabled = false;
             buttonCall.Enabled = false;
             FormHall callHall = new FormHall();
